d2be0f3bc761efeea58c49f750b33f002927bb0d
codepilgrim0 <codepilgrim0@proton.me>
2025-09-28 16:24:43 +0100

    Ensure Telescope shwos hidden files

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>.", "<CMD>Oil<CR>", { desc = "Launch Oil" })
+vim.keymap.set("n", "<leader><leader>", "<CMD>Telescope find_files<CR>", { desc = "Find files in project" })
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require('telescope').setup {
+      defaults = {
+        file_ignore_patterns = {
+          ".git/*",
+        },
+      },
+      pickers = {
+        find_files = {
+          hidden = true, -- Include hidden files
+        },
+      },
+    }
+  end,
+}
