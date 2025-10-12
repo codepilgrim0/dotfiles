788f986f057f9ed778336e1086c6e4aeb02e126f
codepilgrim0 <codepilgrim0@proton.me>
2025-10-12 11:36:28 +0100

    Ensure mason installs the required lsps

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+return {
+  "mason-org/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shellcheck",
+      "shfmt",
+      "flake8",
+      "jdtls",
+      "lua-language-server",
+      "lua_ls",
+      "shfmt",
+      "stylua",
+    },
+  },
+}
